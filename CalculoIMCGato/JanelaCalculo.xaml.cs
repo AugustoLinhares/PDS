1f48326b4fa0b3dd039cf632c31cce07851b83a7
AugustoLinhares <augustol.j.ignacio@gmail.com>
2023-04-11 11:54:12 -0400

    CalculoIMC(Gato)

---
+++ CalculoIMCGato/JanelaCalculo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CalculoIMCGato
+{
+    /// <summary>
+    /// Lógica interna para JanelaCalculo.xaml
+    /// </summary>
+    public partial class JanelaCalculo : Window
+    {
+        public JanelaCalculo()
+        {
+            InitializeComponent();
+        }
+    }
+}
